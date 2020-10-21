ca6bfd958e81bbfd1dbe9a7d4b888c76332d9731
Paul Lesack <paul.lesack@ubc.ca>
2020-10-21 15:56:35 -0700

    dataverse JSON is coming along.

--- dryad2dataverse/dryad.py
+++ dryad2dataverse/dryad.py
@@ -1,7 +1,5 @@
 '''Import and convert dryad metadata'''
 
-TEST=2
-
 class Dryad(object):
     def __init__(self, doi):
         '''
@@ -9,7 +7,26 @@ class Dryad(object):
             DOI of Dryad study. Required for downloading
         '''
         self.doi = doi
-        self.dryJson = None
+        self._dryadJson = None
+    
+    def fetch_record(self):
+        '''
+        Download Dryad JSON and save to Dryad.dryadJson
+        '''
+        pass
+
+    def _typeclass(self, typeName, multiple, typeClass):
+        '''
+        Creates wrapper around single or multiple Dataverse JSON objects.
+        Returns a dict *without* the 'value' key'
+        
+        typeName : str
+            Dataverse typeName (eg: 'author')
+        multiple : boolean
+        typeClass : str
+            Dataverse typeClass. Usually one of 'compound', 'primitive, 'controlledVocabulary')
+        '''
+        return {'typeName':typeName, 'multiple':multiple, 'typeClass':typeClass}
 
     def _convert_generic(self, **kwargs):
         '''
@@ -17,42 +34,59 @@ class Dryad(object):
             {dvField:
                 {'typeName': dvField,
                   'value': dryField}
-        Suitable for generalized conversions.
-        
+        Suitable for generalized conversions. Only provides fields with multiple: False and typeclass:Primitive
+            
         dvField : str
             Dataverse output field
         dryField : str
             Dryad JSON field to convert
         inJson : dict
             JSON segment to convert
+        addJSON : dict (optional)
+            any other JSON required to complete (cf ISNI)
+        rType : str
+            'dict' (default) or 'list'
+            returns 'value' field as dict value or list.
+        pNotes : str
+            Notes to be prepended to list type values. No trailing space required.
         '''
 
         dvField = kwargs.get('dvField')
         dryField = kwargs.get('dryField')
         inJson = kwargs.get('inJson')
+        addJson = kwargs.get('addJson')
+        pNotes = kwargs.get('pNotes', '')
+        rType = kwargs.get('rType', 'dict')
         if not dvField or not dryField or not inJson:
             raise TypeError('Incorrect or insufficient fields provided')
         outfield = inJson.get(dryField)
+        if outfield: outfield = outfield.strip()
+        '''
         if not outfield:
             raise ValueError(f'Dryad field {dryField} not found')
-
-        return {dvField:
+        '''
+        # If value missing can still concat empty dict
+        if not outfield: return {}
+        if rType == 'list':
+            if pNotes:
+                outfield = [f'{pNotes} {outfield}'] 
+       
+        outJson = {dvField:
                         {'typeName':dvField,
-                         'value': outfield}}
+                          'multiple': False, 
+                          'typeClass':'primitive',
+                          'value': outfield}}
+        #Simple conversion
+        if not addJson:
+            return outJson
 
-    def _convert_abstract(self, abstract):
-        '''
-        abstract : str
-            dryad['abstract']
-
-        From examination of UBCs dois, abstract does not repeat.
-        All data sets have one, so presumably it's mandatory
-        '''
-        return {'dsDescriptionValue':{'typeName':'dsDescriptionValue', 'value': abstract}}
+        #Add JSONs together
+        addJson.update(outJson)
+        return addJson
 
     def _convert_author_names(self, author):
         '''
-        Produces required author json fields
+        Produces required author json fields. Special csase, requires concatenation of several fields
         author : dict
             dryad['author']
         '''
@@ -61,177 +95,187 @@ class Dryad(object):
         if first + last == None:
             return None
         authname = f"{author.get('lastName','')}, {author.get('firstName', '')}"
-        return {'authorName':{'typeName':'authorName', 'value': f'{authname}'}}
+        return {'authorName':{'typeName':'authorName', 'value': authname, 'multiple':False, 'typeClass':'primitive'}}
     
-    def _convert_email(self, author):
+    def _convert_keywords(self, *args):
         '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-        email = author.get('email')
-        if not email: return None
-        return  {'datasetContactEmail': 
-            {'typeName':'datasetContactEmail',
-                'value':email}}        
+        Produces the insane keyword structure from a list of words
+        args : list with str elements
+            remember to expand with *
 
-    def _convert_orcid(self, author):
         '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
+        outlist = []
+        for arg in args:
+            outlist.append({'keywordValue': {
+                'typeName':'keywordValue',
+                'value': arg}})
+        return outlist
+
+    def _convert_notes(self, dryJson):
+        #multiple fields must be concatenated into one.
+        notes = ''
+        #these fields should be concatenated into notes
+        notable = ['versionNumber', 'manuscriptNumber',
+                   'usageNotes', 'methods','preserveCurationStatus',
+                   'invoiceId']
+        for note in notable:
+            text = dryJson.get(note)
+            if text:
+                if note =='versionNumber':
+                    text = f'Dryad version number: {text}'
+                if note == 'manuscriptNumber':
+                    text == f'Manuscript number: {text}'
+                if note == 'preserveCurationStatus':
+                    text = f'Preserve curation status: {text}'
+                if note == 'invoiceId':
+                    text == f'Invoice ID: {text}'
+                text = text.strip()
+                notes += f'<p>{text}</p>\n'
+        concat = {'typeName':'notesText',
+                  'value': notes}
+        return concat
+
+    def _assemble_json(self, dryJson=None):
         '''
-        orcid = author.get('orcid')
-        if not orcid: return None
-        return {'authorIdentifierScheme': 
+        Assembles Dataverse json from Dryad components
+
+        dryJson : dict
+            Dryad json as dict
+        '''
+        if not dryJson:
+            dryJson = self._dryadJson
+        #print(dryJson)
+        self.dvJson ={'datasetVersion':
+                {'license':'CC0',
+                 'termsOfUse': 'CC0 Waiver',
+                 'metadataBlocks':{'citation':
+                                    {'displayName': 'Citation Metadata',
+                                      'fields': []},
+                                   
+                                   }
+                 }
+                }
+        '''
+        REQUIRED Dataverse fields
+
+        Dryad is a general purpose database; it is hard/impossible to get
+        Dataverse required subject tags out of their keywords, so:
+        '''
+        defaultSubj = {'typeName' : 'subject', 
+                       'typeClass':'controlledVocabulary',
+                       'multiple': True,
+                       'value' : ['Other']}
+        self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(defaultSubj) 
+
+        reqdTitle = self._convert_generic(inJson=dryJson,
+                                         dryField='title',
+                                         dvField='title')['title']
+        
+        self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(reqdTitle) 
+       
+        ##rewrite as function: authors
+        out = []
+        for a in dryJson['authors']:
+            reqdAuthor = self._convert_author_names(a)
+            if reqdAuthor:
+                affiliation = self._convert_generic(inJson=a, 
+                                                dvField='authorAffiliation',
+                                                dryField='affiliation')
+                addOrc={'authorIdentifierScheme': 
                 {'typeName':'authorIdentifierScheme', 
-                    'value': 'ORCID'},
-               'authorIdentifier': 
-               {'typeName':'authorIdentifier',
-                   'value':'0000-0003-3451-4803'}
-               }
+                    'value': 'ORCID',
+                    'typeClass': 'controlledVocabulary',
+                    'multiple':False}}
+                #only ORCID at UBC
+                orcid = self._convert_generic(inJson=a, 
+                                                dvField='authorIdentifier',
+                                                dryField='orcid',
+                                                addJson=addOrc)
+                if affiliation: reqdAuthor.update(affiliation)
+                if orcid: reqdAuthor.update(orcid)
+                out.append(reqdAuthor)
 
-    def _convert_affiliation(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-        affiliation = author.get('affiliation')
-        if not affiliation: return None
-        return {'authorAffiliation': 
-               {'typeName':'authorAffiliation', 'value': affiliation}
-            }
+        authors= self._typeclass(typeName='author', multiple=True, typeClass='compound')
+        authors['value'] = out
 
-    def _convert_isni(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
+        self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(authors)
 
-    def _convert_org(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
 
-    def _convert_award(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
+        ##rewrite as function:contact
+        out = []
+        for e in dryJson['authors']:
+            reqdContact=self._convert_generic(inJson=e, 
+                                              dvField='datasetContactEmail', 
+                                              dryField='email')
+            if reqdContact:
+                author = self._convert_author_names(e)
+                author = {'author':author['authorName']['value']}#for passing to function
+                author = self._convert_generic(inJson=author,
+                                               dvField='datasetContactName',
+                                               dryField='author')
+                if author: reqdContact.update(author)
+                affiliation = self._convert_generic(inJson=e, 
+                                                dvField='datasetContactAffiliation',
+                                                dryField='affiliation')
+                if affiliation: reqdContact.update(affiliation)
+                out.append(reqdContact)
+        contacts = self._typeclass(typeName='datasetContact', multiple=True, typeClass='compound')
+        contacts['value'] = out
+        self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(contacts)
+            
 
-    def _convert_ident(self, author):
-        '''
-        Return DOI here
-        author : dict
-            dryad['author']
-        '''
+        #Description
+        description = self._typeclass(typeName='dsDescription', multiple=True, typeClass='compound')
+        descrField = self._convert_generic(inJson=dryJson, dvField='dsDescriptionValue', dryField='abstract')
+        description['value'] = [descrField] ##FFFFUUUU
+        self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(description)
 
-    def _convert_invoice_id(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
+        #Granting agencies
+        if dryJson.get('funders'):
 
-    def _convert_keywords(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
+            out = []
+            for fund in dryJson['funders']:
+                org = self._convert_generic(inJson=fund, 
+                                           dvField='grantNumberAgency', 
+                                           dryField='organization')
+                if fund.get('awardNumber'):
+                    fund = self._convert_generic(inJson=fund, 
+                                               dvField='grantNumberValue', 
+                                               dryField='awardNumber')
+                    org.update(fund) 
+                out.append(org)
+            grants = self._typeclass(typeName='grantNumber', multiple=True, typeClass='compound')
+            grants['value'] = out 
+            self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(grants)
 
-    def _convert_last_mod(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
+        #Keywords
+            keywords = self._typeclass(typeName='keyword', multiple=True, typeClass='compound')
+            out = []
+            for key in dryJson['keywords']:
+                keydict = {'keyword':key}#because takes a dict
+                kv = self._convert_generic(inJson=keydict,
+                                           dvField='keywordValue',
+                                           dryField='keyword')
+                vocab = {'dryad':'Dryad'}
+                voc = self._convert_generic(inJson=vocab,
+                                            dvField='keywordVocabulary',
+                                            dryField='dryad')
+                kv.update(voc)
+                out.append(kv)
+            keywords['value'] = out
+            self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(keywords)
 
-    def _convert_license(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
+        #distribution date
+        moddate = self._convert_generic(inJson=dryJson,
+                                            dvField='dateOfDeposit',
+                                            dryField='lastModificationDate')
+        self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(moddate)
 
-    def _convert_locations(self, author):
-        '''
-        Geospatial data here. A nightmare
-        author : dict
-            dryad['author']
-        '''
+        #distribution date
+        distdate = self._convert_generic(inJson=dryJson,
+                                            dvField='distributionDate',
+                                            dryField='publicationDate')
+        self.dvJson['datasetVersion']['metadataBlocks']['citation']['fields'].append(distdate)
 
-    def _convert_methods(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_pubdate(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_issn(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_pubname(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_relatedworks(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_links(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_title(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_usage_notes(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_userid(self, author):
-        '''
-        This one may be useless
-        author : dict
-            dryad['author']
-        '''
-
-    def _convert_version_no(self, author):
-        '''
-        Produces email dataverse email json
-        author : dict
-            dryad['author']
-        '''
+        ##TODO create minimal JSON as test for upload
+        
--- tests/dryad2dataverse_tests.py
+++ tests/dryad2dataverse_tests.py
@@ -12,11 +12,29 @@ testCase.dryJson = json.loads(cursor.execute(f'SELECT json from dryadjson WHERE 
 with open('/Users/paul/Documents/Work/Projects/dryad2dataverse/tmp/one.pickle', 'rb') as f:
     testCase.dryJson = pickle.load(f)
 
+NULLVALUES= {x[0]: None for x in testCase.dryJson['authors'][0].items()}
+
+NOMULT ={'multiple':False, 'typeClass':'primitive'}
+
 def setup():
     print( "Start test")
 
     def teardown():
         print( "Finish ")
+
+
+def test_generic():
+    #Test on affiliation
+    out = {'authorAffiliation':{'typeName':'authorAffiliation', 'value':'University of Minnesota', 'multiple':False, 'typeClass':'primitive'}}
+    assert_equal(isinstance(testCase.dryJson['authors'][0], dict), True)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson['authors'][0], 
+                dvField='authorAffiliation',
+                dryField='affiliation'), out)
+
+    # Test NULLVALUES
+    assert_equal(testCase._convert_generic(inJson=NULLVALUES,
+                dvField='authorAffiliation',
+                dryField='affiliation'), {})
 
 
 @with_setup(setup)
@@ -25,46 +43,149 @@ def test_abstract():
 
     abs = {'dsDescriptionValue': 
             {'typeName':'dsDescriptionValue', 
-              'value':abstract}}
+              'value':abstract, 'multiple':False, 'typeClass':'primitive'}}
     assert_equal(isinstance(testCase.dryJson['abstract'], str), True)
-    assert_equal(testCase._convert_abstract(testCase.dryJson['abstract']), abs)
+    #assert_equal(testCase._convert_abstract(testCase.dryJson['abstract']), abs)
     assert_equal(testCase._convert_generic(inJson=testCase.dryJson, dvField='dsDescriptionValue', dryField='abstract'), abs)
 
 def test_author():
     #dvAuthor = json.loads("{'authorName':{'typeName':'authorName', 'value': 'Powers, Jennifer'}}")
-    dvAuthor = {'authorName':{'typeName':'authorName', 'value': 'Powers, Jennifer'}}
+    dvAuthor = {'authorName':{'typeName':'authorName', 'value': 'Powers, Jennifer','multiple':False, 'typeClass':'primitive'}}
     assert_equal(isinstance(testCase.dryJson['authors'][0], dict), True)
     assert_equal(testCase._convert_author_names(testCase.dryJson['authors'][0]), dvAuthor)
 
 def test_email():
     email = {'datasetContactEmail': 
             {'typeName':'datasetContactEmail',
-                'value':'powers@umn.edu'}}
+                'value':'powers@umn.edu','multiple':False, 'typeClass':'primitive'}}
     isinstance(testCase.dryJson['authors'][0], dict)
-    assert_equal(isinstance(testCase.dryJson['authors'][0], dict), True)
-    assert_equal(testCase._convert_email(testCase.dryJson['authors'][0]), email)
-
+    #assert_equal(isinstance(testCase.dryJson['authors'][0], dict), True)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson['authors'][0], dvField='datasetContactEmail', dryField='email'), email)
 
 def test_orcid():
-    orcid = {'authorIdentifierScheme': 
-               {'typeName':'authorIdentifierScheme', 'value': 'ORCID'},
-               'authorIdentifier': {'typeName':'authorIdentifier','value':'0000-0003-3451-4803'}
-               }
+    addJson={'authorIdentifierScheme': 
+                {'typeName':'authorIdentifierScheme', 
+                    'value': 'ORCID',
+                    'typeClass': 'controlledVocabulary',
+                    'multiple':False}}
+
+
+    orcid = {'authorIdentifier': {'typeName':'authorIdentifier','value':'0000-0003-3451-4803','multiple':False, 'typeClass':'primitive'}}
+    orcid.update(addJson)
     isinstance(testCase.dryJson['authors'][0], dict)
+    assert_equal(isinstance(addJson, dict),True)
     assert_equal(isinstance(testCase.dryJson['authors'][0], dict), True)
-    assert_equal(testCase._convert_orcid(testCase.dryJson['authors'][0]),orcid)
+    #assert_equal(testCase._convert_orcid(testCase.dryJson['authors'][0]),orcid)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson['authors'][0], dvField='authorIdentifier', dryField='orcid', addJson=addJson),orcid)
 
 def test_affiliation():
     affil = {'authorAffiliation': 
                {'typeName':'authorAffiliation', 'value': 'University of Minnesota'}
             }
+    affil['authorAffiliation'].update(NOMULT)
     assert_equal(isinstance(testCase.dryJson['authors'][0], dict), True)
-    assert_equal(testCase._convert_affiliation(testCase.dryJson['authors'][0]),affil)
+    #assert_equal(testCase._convert_affiliation(testCase.dryJson['authors'][0]),affil)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson['authors'][0], dvField='authorAffiliation', dryField='affiliation'),affil)
 
-def test_generic():
-    #Test on affiliation
-    out = {'authorAffiliation':{'typeName':'authorAffiliation', 'value':'University of Minnesota'}}
+def test_isni():
+    '''Dryad has no ISNI'''
+    addJson={'authorIdentifierScheme': 
+                {'typeName':'authorIdentifierScheme', 
+                    'value': 'ISNI'}}
+    addJson['authorIdentifierScheme'].update(NOMULT)
+
+    ident = {'authorIdentifier': {'typeName':'authorIdentifier','value':'0000-0003-3451-4803'}
+               }
+    ident['authorIdentifier'].update(NOMULT)
+       
+    assert_equal(isinstance(addJson, dict),True)
     assert_equal(isinstance(testCase.dryJson['authors'][0], dict), True)
-    assert_equal(testCase._convert_generic(inJson=testCase.dryJson['authors'][0], 
-                dvField='authorAffiliation',
-                dryField='affiliation'), out)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson['authors'][0], dvField='authorIdentifier', dryField='affiliationISNI', addJson=addJson),{})
+
+def test_funders():
+    funder = {'grantNumberValue': 
+               {'typeName':'grantNumberValue', 'value': 'CAREER grant DEB 1053237'}}
+    funder['grantNumberValue'].update(NOMULT)
+    assert_equal(isinstance(testCase.dryJson['funders'][0], dict), True)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson['funders'][0], dvField='grantNumberValue', dryField='awardNumber') , funder)
+
+def test_doi():
+    doi  = {'relatedDatasets': 
+            {'typeName':'relatedDatasets', 'value': ['Original dataset in Dryad repository: doi:10.5061/dryad.2rbnzs7jp']}}
+    doi['relatedDatasets'].update(NOMULT)
+    pnotes= 'Original dataset in Dryad repository:'
+
+    
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson, 
+        dvField='relatedDatasets', 
+        dryField='identifier', 
+        addJson=None,
+        pNotes=pnotes,
+        rType='list'
+        ), doi)
+
+def test_keyword():
+    keywords=[
+            {'keywordValue':{'typeName':'keywordValue','value':'drought'}},
+            {'keywordValue':{'typeName':'keywordValue','value':'Tree mortality'}},
+            {'keywordValue':{'typeName':'keywordValue','value':'Costa Rica'}}
+            ]
+
+    assert_equal(isinstance(testCase.dryJson['keywords'], list), True)
+    assert_equal(testCase._convert_keywords(*testCase.dryJson['keywords']), keywords)
+
+def test_lastmod():
+    lastmod= {'distributionDate': 
+            {'typeName':'distributionDate',
+                'value':'2020-03-17'}.update(NOMULT)}
+
+def test_notes():
+    assert_equal.__self__.maxDiff = None
+    notes = {'typeName': 'notesText', 'value': '<p>Dryad version number: 4</p>\n<p><p>All data and associated R scripts are contained in the files. The R scripts refer to the three main questions asked in the associated publication.</p></p>\n<p><p>The dataset contains annual observations of tree mortality in different 18 plots, expanded sampling for mortality during 2015, and datasets of plant functional and hydraulic traits.\xa0 Please refer to the publication for exact methods.\xa0 All data come from Guanacaste, Costa Rica.</p></p>\n'}
+    assert_equal(testCase._convert_notes(testCase.dryJson), notes )
+
+def test_pub_date():
+    pubdate = {'distributionDate': 
+               {'typeName':'distributionDate', 'value': '2020-03-17'}}
+    pubdate['distributionDate'].update(NOMULT)
+
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson, 
+            dvField='distributionDate', 
+            dryField='publicationDate') , pubdate)
+
+def test_issn():
+    
+    addJson = {'publicationIDType': 
+               {'typeName':'publicationIDType', 'value': 'issn'}}
+    addJson['publicationIDType'].update(NOMULT)
+    addJson['publicationIDType']['typeClass']='controlledVocabulary' #remember to do this
+    issn = {'publicationIDNumber':
+            {'typeName':'publicationIDNumber', 'value':'TEST'}}
+    issn['publicationIDNumber'].update(NOMULT)
+
+    testCase.dryJson.update({'publicationISSN': 'TEST'})
+    out = addJson.copy()
+    out.update(issn)
+    
+    assert_equal(isinstance(out, dict), True)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson, 
+        dvField='publicationIDNumber', 
+        dryField='publicationISSN', 
+        addJson=addJson,
+        ), out)
+    del testCase.dryJson['publicationISSN']
+
+
+def test_title():
+    #TITLES don't actually have the 'title' prepended BFY
+    out= {'title': 
+            {'typeName':'title',
+                'value':'A catastrophic tropical drought kills hydraulically vulnerable tree species'}}
+    out['title'].update(NOMULT)
+    assert_equal(testCase._convert_generic(inJson=testCase.dryJson, 
+        dvField='title', 
+        dryField='title', 
+        addJson=None,
+        ), out)
+
+
