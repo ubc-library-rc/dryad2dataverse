7888caefd896a96d24c84eb8bf2a6f09a5630b7d
Paul Lesack <paul.lesack@ubc.ca>
2026-02-02 11:03:28 -0800

    Fixed url typo, create default config

--- src/dryad2dataverse/__init__.py
+++ src/dryad2dataverse/__init__.py
@@ -26,7 +26,7 @@ downloading and file duplication.
 
 import sys
 
-VERSION = (0, 8, 0)
+VERSION = (0, 8, 1)
 __version__ = '.'.join([str(x) for x in VERSION])
 USERAGENT = (f'dryad2dataverse/v{__version__} ({sys.platform.capitalize()}); '
              f'Python {sys.version[:sys.version.find("(")-1]}')
